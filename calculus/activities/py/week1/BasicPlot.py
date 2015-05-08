d5fdf5e022a1256b60f9e79fede769f0807a667e
KellyBlack <kjblack@gmail.com>
2015-05-08 09:02:06 -0400

    cleaned up the pre class graphing script to make better use of the graphing class

--- calculus/activities/py/week1/BasicPlot.py
+++ calculus/activities/py/week1/BasicPlot.py
@@ -15,6 +15,10 @@ class BasicPlot:
     def addFunction(self,domain,range,format,width=2.0):
         plt.plot(domain,range,format,linewidth=width)
 
+    def setAxesBounds(self,lowx,highx,lowy,highy):
+        plt.xlim([lowx,highx])
+        plt.ylim([lowy,highy])
+
     def setupGrid(self,thickness=1.0,style='--',
                   startx=0.0,deltax=0.1,endx=1.0,
                   starty=0.0,deltay=0.1,endy=1.0):
@@ -36,6 +40,8 @@ class BasicPlot:
         ax.set_yticks(np.arange(startx,endx,deltax))
         ax.set_xticks(np.arange(starty,endy,deltay))
 
+        ax.xaxis.grid(linewidth=thickness)
+        ax.yaxis.grid(linewidth=thickness)
         plt.grid(True)
 
     def axesDecorations(self,title,horiztonalLabel,verticallabel):
@@ -44,7 +50,27 @@ class BasicPlot:
         plt.ylabel(verticallabel)
         plt.title(title)
 
+    def clearPlot(self):
+        plt.clf()
 
 
 if (__name__ =='__main__') :
-    pass
+    plotter = BasicPlot()
+
+    #plt.xkcd(randomness=0.1)
+    #with plt.xkcd():
+    plotter.clearPlot()
+
+    with plt.xkcd():
+        plotter.addFunction([0,1,2,3],[0,-1.0,2,1], 'k-',2.0)
+        plotter.setupGrid(0.5,'--',
+                            -1.0,0.5,2.0,
+                            0.0,1.0,3.0)
+        plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
+        #plt.text(1.0,3.025, r'$\alpha=100,\ \sigma=15$'
+
+
+    plotter.setAxesBounds(0.0,3.05,-1.05,2.05)
+    plt.show()
+    #plt.savefig('preClass3.pgf')
+
--- calculus/activities/py/week1/preClass.py
+++ calculus/activities/py/week1/preClass.py
@@ -9,7 +9,7 @@ from BasicPlot import BasicPlot
 # Make the first plot
 plotter = BasicPlot()
 plotter.addFunction([0,1,2,3],[0,1,1,2], 'k-',2.0)
-plotter.setupGrid(3.0,'--',
+plotter.setupGrid(0.5,'--',
                   0.0,0.25,2.5,
                   0.0,0.5,3.0)
 plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
@@ -19,60 +19,24 @@ plt.savefig('preClass1.pgf')
 
 
 # Make the second plot
-plt.clf()
-plt.plot([0,1,2,3],[0,0.5,3,2], 'k-',linewidth=2.0)
-ax = plt.gca()
-xticklines = plt.getp(ax, 'xticklines')
-yticklines = plt.getp(ax, 'yticklines')
-xgridlines = plt.getp(ax, 'xgridlines')
-ygridlines = plt.getp(ax, 'ygridlines')
-xticklabels = plt.getp(ax, 'xticklabels')
-yticklabels = plt.getp(ax, 'yticklabels')
-
-plt.setp(xticklines, 'linewidth', 1)
-plt.setp(yticklines, 'linewidth', 1)
-plt.setp(xgridlines, 'linestyle', '--')
-plt.setp(ygridlines, 'linestyle', '--')
-plt.setp(yticklabels, 'color', 'k', fontsize='medium')
-plt.setp(xticklabels, 'color', 'k', fontsize='medium')
-ax.set_yticks(np.arange(0.0,3.0,0.2))
-ax.set_xticks(np.arange(0,3.0,0.5))
-
-plt.grid(True)
-plt.axis([0.0, 3.0, 0.0, 3.0])
-plt.xlabel('Time (sec)')
-plt.ylabel('Position (m)')
-plt.title('Position of an Object')
+plotter.clearPlot()
+plotter.addFunction([0,1,2,3],[0,0.5,3,2], 'k-',2.0)
+plotter.setupGrid(0.5,'--',
+                  0.0,0.2,3.0,
+                  0.0,0.5,3.0)
+plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
 #plt.text(1.0,3.025, r'$\alpha=100,\ \sigma=15$')
 plt.savefig('preClass2.pgf')
 #plt.show()
 
 
 # Make the third plot
-plt.clf()
-plt.plot([0,1,2,3],[0,-1.0,2,1], 'k-',linewidth=2.0)
-ax = plt.gca()
-xticklines = plt.getp(ax, 'xticklines')
-yticklines = plt.getp(ax, 'yticklines')
-xgridlines = plt.getp(ax, 'xgridlines')
-ygridlines = plt.getp(ax, 'ygridlines')
-xticklabels = plt.getp(ax, 'xticklabels')
-yticklabels = plt.getp(ax, 'yticklabels')
-
-plt.setp(xticklines, 'linewidth', 1)
-plt.setp(yticklines, 'linewidth', 1)
-plt.setp(xgridlines, 'linestyle', '--')
-plt.setp(ygridlines, 'linestyle', '--')
-plt.setp(yticklabels, 'color', 'k', fontsize='medium')
-plt.setp(xticklabels, 'color', 'k', fontsize='medium')
-ax.set_yticks(np.arange(-1.0,2.0,0.2))
-ax.set_xticks(np.arange(0,3.0,0.5))
-
-plt.grid(True)
-plt.axis([0.0, 3.0, -1.0, 2.0])
-plt.xlabel('Time (sec)')
-plt.ylabel('Position (m)')
-plt.title('Position of an Object')
+plotter.clearPlot()
+plotter.addFunction([0,1,2,3],[0,-1.0,2,1], 'k-',2.0)
+plotter.setupGrid(0.5,'--',
+                 -1.0,0.2,2.0,
+                  0.0,0.5,3.0)
+plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
 #plt.text(1.0,3.025, r'$\alpha=100,\ \sigma=15$'
 plt.savefig('preClass3.pgf')
 #plt.show()
