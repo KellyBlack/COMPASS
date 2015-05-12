0ca73fae9d5097c0f3f58f084e5a42d0db7f26c3
KellyBlack <kjblack@gmail.com>
2015-05-12 08:15:38 -0400

    Added a blank python script for the seond day of week 2

---
+++ calculus/activities/py/week2/day2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+sys.path.insert(0, '../')
+from BasicPlot import BasicPlot
+
+plotter = BasicPlot()
+
+###############################
+plotter.clearPlot()
+
