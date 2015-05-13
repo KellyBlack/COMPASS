cdfffaf063ae289b6e42fd8e2712bdc541f4d92d
KellyBlack <kjblack@gmail.com>
2015-05-13 08:46:14 -0400

    Added the header for the new python script

---
+++ calculus/activities/py/week3/day1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+sys.path.insert(0, '../')
+from BasicPlot import BasicPlot
+
+plotter = BasicPlot()
+
+
+
+
+###############################
+plotter.clearPlot()
+
+
+
