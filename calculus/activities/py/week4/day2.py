69537d25c28ac4323187487b16896b72cfb3490c
KellyBlack <kjblack@gmail.com>
2015-05-14 12:13:52 -0400

    Added a blank python script for the intro to trig functions

---
+++ calculus/activities/py/week4/day2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.patches import FancyArrowPatch
+import sys
+
+
+sys.path.insert(0, '../')
+from BasicPlot import BasicPlot
+
+plotter = BasicPlot()
+
+
+
+
+###############################
+plotter.clearPlot()
+
+
+
