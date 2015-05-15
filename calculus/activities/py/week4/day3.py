70d12920f64664d42e690c4ca2c82fc882a4a609
KellyBlack <kjblack@gmail.com>
2015-05-15 10:34:19 -0400

    Updated the python script for generating an image of the grid used for the path

--- calculus/activities/py/week4/day3.py
+++ calculus/activities/py/week4/day3.py
@@ -14,5 +14,21 @@ from BasicPlot import BasicPlot
 
 plotter = BasicPlot()
 
+###############################
+plotter.clearPlot()
+
+plotter.setAxes(True)
+axis = plotter.getAxes()
+plotter.setAxesBounds(-5.0,5.0,-5.0,5.0)
+plotter.setupGrid(0.3,'--',
+                  -5.0,1.0,5.1,
+                  -5.0,1.0,5.1)
+plotter.axesDecorations('Position of an Object','x (m)','y (m)')
+
+
+plt.draw()
+#plt.show()
+plt.savefig('path_week4day3.pgf',format='pgf')
+
 
 
