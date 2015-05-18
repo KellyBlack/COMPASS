9bbe99a75d20834f04a8c059e2531b1ede1a3aee
KellyBlack <kjblack@gmail.com>
2015-05-18 13:26:13 -0400

    Added a python script to generate images to be used for working with the derivative of sin(t)$

--- calculus/activities/py/week6/day2.py
+++ calculus/activities/py/week6/day2.py
@@ -21,5 +21,68 @@ plotter.clearPlot()
 plotter.setAxes(True)
 axis = plotter.getAxes()
 
+plotter.subplot(2,1,1)
+
+plotter.setAxesBounds(0.0,2.0*np.pi,-1.1,1.1)
+plotter.setupGrid(0.3,'--',
+                  -0.0,np.pi*0.5,2.0*np.pi+.01,
+                   -1.0,0.5,1.1)
+plotter.xAxisTicks([0.0,np.pi*0.5,np.pi,np.pi*1.5,np.pi*2.0],
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$"])
+plotter.axesDecorations('Velocity of an Object','','x (m)')
+
+
+plotter.subplot(2,1,2)
+
+t = np.linspace(0.0,2.0*np.pi,40)
+plotter.addFunction(t,np.sin(t),'k-',2.0)
+
+plotter.setAxesBounds(0.0,2.0*np.pi,-1.1,1.1)
+plotter.setupGrid(0.3,'--',
+                  -0.0,np.pi*0.5,2.0*np.pi+.01,
+                   -1.0,0.5,1.1)
+plotter.xAxisTicks([0.0,np.pi*0.5,np.pi,np.pi*1.5,np.pi*2.0],
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$"])
+plotter.axesDecorations('Position of an Object','t (s)','x (m)')
+
+
+plt.draw()
+#plt.show()
+plt.savefig('sineInTime_week6day2.pgf',format='pgf')
+
+###############################
+plotter.clearPlot()
+
+plotter.setAxes(True)
+axis = plotter.getAxes()
+
+plotter.subplot(2,1,1)
+
+plotter.setAxesBounds(0.0,2.0*np.pi,-1.1,1.1)
+t = np.linspace(0.0,2.0*np.pi,40)
+plotter.addFunction(t,np.sin(t),'k-',2.0)
+plotter.setupGrid(0.3,'--',
+                  -0.0,np.pi*0.5,2.0*np.pi+.01,
+                   -1.0,0.5,1.1)
+plotter.xAxisTicks([0.0,np.pi*0.5,np.pi,np.pi*1.5,np.pi*2.0],
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$"])
+plotter.axesDecorations('Velocity of an Object','','x (m)')
+
+
+plotter.subplot(2,1,2)
+
+plotter.setAxesBounds(0.0,2.0*np.pi,-1.1,1.1)
+plotter.setupGrid(0.3,'--',
+                  -0.0,np.pi*0.5,2.0*np.pi+.01,
+                   -1.0,0.5,1.1)
+plotter.xAxisTicks([0.0,np.pi*0.5,np.pi,np.pi*1.5,np.pi*2.0],
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$"])
+plotter.axesDecorations('Position of an Object','t (s)','x (m)')
+
+
+plt.draw()
+#plt.show()
+plt.savefig('sineInTimeVel_week6day2.pgf',format='pgf')
+
 
 
