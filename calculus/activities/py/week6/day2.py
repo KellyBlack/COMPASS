5223b113654e0f4c4ca2764de4510de4f7eab8f0
KellyBlack <kjblack@gmail.com>
2015-05-18 12:31:10 -0400

    Added a template python script for plotting trig functions

---
+++ calculus/activities/py/week6/day2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.path as path
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Ellipse
+import math
+
+import sys
+
+
+sys.path.insert(0, '../')
+from BasicPlot import BasicPlot
+
+plotter = BasicPlot()
+
+###############################
+plotter.clearPlot()
+
+plotter.setAxes(True)
+axis = plotter.getAxes()
+
+
+
