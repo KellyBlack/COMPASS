67d1e464d5645513b20b521a13ef897404030a90
KellyBlack <kjblack@gmail.com>
2016-08-22 11:00:07 -0400

    Added new velocity position graph for limits and derivatives

--- calculus/activities/semI/py/week2/day2.py
+++ calculus/activities/semI/py/week2/day2.py
@@ -12,6 +12,40 @@ plotter = BasicPlot()
 ###############################
 plotter.clearPlot()
 
+# Make the plot for the velocity
+plotter.subplot(2,1,1)
+
+t1 = np.arange(0,4.05,0.1)
+t2 = np.arange(4,5.01,0.02)
+with plt.xkcd():
+    plotter.addInterpolant([[0,2.0],[1,0.0],[2.0,-2],
+                            [3.0,0.0],[4,2.0]],t1,'k-',2.0)
+    plotter.addInterpolant([[4,-1.0],[4.5,0.0],[4.5,0.0],
+                            [5.0,-1.0]],t2,'k-',2.0)
+
+    plotter.markJumps([[0,2,True],[4,2,True],
+                       [4,-1,False],[5,-1,True],
+        ],8.0)
+    plotter.setupGrid(0.3,'--',
+                      0.0,1.0,5.1,
+                      -2.0,1.0,2.1)
+    plotter.setAxesBounds(-0.1,5.1,-2.1,2.1)
+    plotter.axesDecorations('Velocity of an Object','','Velocity (m/s)')
+
+## make the empty plot for the distance
+plotter.subplot(2,1,2)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,5.1,
+                  -4.0,1.0,4.1)
+plotter.setAxesBounds(-0.1,5.1,-4.1,4.1)
+plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
+
+#plt.show()
+plt.savefig('vel2Pos_introDerivLimit.pgf',format='pgf')
+
+###############################
+plotter.clearPlot()
+
 def f(t):
     return(-7/6.0*(t)**3.0+24.0/6.0*t*t-11.0/6.0*t)
 
