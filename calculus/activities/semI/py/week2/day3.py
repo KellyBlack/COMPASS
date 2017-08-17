544f1fa8dfc542145e7215d7b29ad360384c3891
KellyBlack <kjblack@gmail.com>
2017-08-17 09:59:42 -0400

    Changed act 5 to be a graphical view of kinematics

--- calculus/activities/semI/py/week2/day3.py
+++ calculus/activities/semI/py/week2/day3.py
@@ -4,12 +4,142 @@ import matplotlib.pyplot as plt
 import sys
 
 
+
 sys.path.insert(0, '../')
 from BasicPlot import BasicPlot
 
 plotter = BasicPlot()
 
+###############################
+plotter.clearPlot()
 
+# Make the plot for the velocity
+plotter.subplot(2,1,1)
+
+t1 = np.arange(0,4.01,0.1)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -4.0,1.0,4.1)
+plotter.setAxesBounds(0.0,4.1,-4.1,4.1)
+plotter.axesDecorations("Moving Object's Kinematics",'','velocity  (m/sec)')
+
+## make the empty plot for the distance
+plotter.subplot(2,1,2)
+plotter.addInterpolant([[0,2],[1,-1],[3,1],[4,-1]],t1,'k-',2.0)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -3.0,2.0,3.1)
+plotter.setAxesBounds(-0.1,4.1,-3.1,3.1)
+plotter.axesDecorations('','Time (sec)','Position (m)')
+
+#plt.show()
+plt.savefig('velocityPlot_Class_5_week2Day3.pgf',format='pgf')
+exit(0)
+
+###############################
+plotter.clearPlot()
+
+# Make the plot for the velocity
+plotter.subplot(2,1,1)
+
+t1 = np.arange(0,4.01,0.1)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -4.0,1.0,4.1)
+plotter.setAxesBounds(0.0,4.1,-4.1,4.1)
+plotter.axesDecorations("Moving Object's Kinematics",'','velocity  (m/sec)')
+
+## make the empty plot for the distance
+plotter.subplot(2,1,2)
+plotter.addInterpolant([[0,-2],[1,0],[2,1],[4,0]],t1,'k-',2.0)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -3.0,2.0,3.1)
+plotter.setAxesBounds(-0.1,4.1,-3.1,3.1)
+plotter.axesDecorations('','Time (sec)','Position (m)')
+
+#plt.show()
+plt.savefig('velocityPlot_Class_4_week2Day3.pgf',format='pgf')
+exit(0)
+
+
+###############################
+plotter.clearPlot()
+
+# Make the plot for the velocity
+plotter.subplot(2,1,1)
+
+t1 = np.arange(0,4.01,0.03)
+plotter.addFunction(t1,2*np.exp(-2.1*(t1-1)*(t1-1))-2,'k-',2.0)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -4.0,1.0,4.1)
+plotter.setAxesBounds(0.0,4.1,-4.1,4.1)
+plotter.axesDecorations("Moving Object's Kinematics",'','velocity  (m/sec)')
+
+## make the empty plot for the distance
+plotter.subplot(2,1,2)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -3.0,2.0,8.1)
+plotter.setAxesBounds(-0.1,4.1,-3.1,8.1)
+plotter.axesDecorations('','Time (sec)','Position (m)')
+
+#plt.show()
+plt.savefig('velocityPlot_Class_3_week2Day3.pgf',format='pgf')
+exit(0)
+
+###############################
+plotter.clearPlot()
+
+# Make the plot for the velocity
+plotter.subplot(2,1,1)
+
+t1 = np.arange(0,4.01,0.1)
+plotter.addInterpolant([[0,3],[1,0],[1,0],[3,2],[4,-2]],t1,'k-',2.0)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -4.0,1.0,4.1)
+plotter.setAxesBounds(0.0,4.1,-4.1,4.1)
+plotter.axesDecorations("Moving Object's Kinematics",'','velocity  (m/sec)')
+
+## make the empty plot for the distance
+plotter.subplot(2,1,2)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -3.0,2.0,8.1)
+plotter.setAxesBounds(-0.1,4.1,-3.1,8.1)
+plotter.axesDecorations('','Time (sec)','Position (m)')
+
+#plt.show()
+plt.savefig('velocityPlot_Class_2_week2Day3.pgf',format='pgf')
+#exit(0)
+
+###############################
+plotter.clearPlot()
+
+# Make the plot for the velocity
+plotter.subplot(2,1,1)
+
+t1 = np.arange(0,4.01,0.1)
+plotter.addInterpolant([[0,4],[2,-3],[4,3]],t1,'k-',2.0)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -4.0,1.0,4.1)
+plotter.setAxesBounds(0.0,4.1,-4.1,4.1)
+plotter.axesDecorations("Moving Object's Kinematics",'','velocity  (m/sec)')
+
+## make the empty plot for the distance
+plotter.subplot(2,1,2)
+plotter.setupGrid(0.3,'--',
+                  0.0,1.0,4.1,
+                  -3.0,2.0,8.1)
+plotter.setAxesBounds(-0.1,4.1,-3.1,8.1)
+plotter.axesDecorations('','Time (sec)','Position (m)')
+
+#plt.show()
+plt.savefig('velocityPlot_Class_1_week2Day3.pgf',format='pgf')
+exit(0)
 
 ###############################
 plotter.clearPlot()
@@ -34,7 +164,6 @@ plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
 
 #plt.show()
 plt.savefig('velocityPlot_week2Day3.pgf',format='pgf')
-exit(0)
 
 
 ###############################
